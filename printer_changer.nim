e83069009b95d87fd623acfc0c2ad41af7bb1263
Duncan Clarke <DClarke3@curtisswright.com>
2025-11-14 09:36:22 -0500

    Changed some icons to work better with dark mode

--- printer_changer.nim
+++ printer_changer.nim
@@ -30,14 +30,23 @@ proc get_default_printer(): string = # get the default printer
   return $(buffer)[0 .. ^2] # trim trailing null character
 
 # load icons
-const exit = staticRead(r"icons/quit.ico") 
+const exit = staticRead(r"icons/goodbye.ico") 
 let exitBitmap = Bitmap(Image(exit).scale(16, 16))
 
-const printer = staticRead(r"icons/printer.ico")
+const printer = staticRead(r"icons/print.ico")
 let printerBitmap = Bitmap(Image(printer).scale(16, 16))
+
+const queue = staticRead(r"icons/queue.ico")
+let queueBitmap = Bitmap(Image(queue).scale(16, 16))
+
+const properties = staticRead(r"icons/gear.ico")
+let propertiesBitmap = Bitmap(Image(properties).scale(16, 16))
 
 const ico = staticRead(r"icons/printer.ico")
 let icon = Icon(ico)
+
+const right = staticRead(r"icons/right.ico")
+let rightBitmap = Bitmap(Image(right).scale(16, 16))
 
 var app = App()
 
@@ -50,41 +59,56 @@ frame.setTrayIcon(icon)
 
 # menu to change default printer
 var menuDefaultPrinter = Menu()
-menuDefaultPrinter.append(idProperties, "Properties", "Default Printer Properties")
-menuDefaultPrinter.append(idQueue, "Queue", "Default Printer Properties")
+menuDefaultPrinter.append(idProperties, "Properties", "Default Printer Properties", propertiesBitmap)
+menuDefaultPrinter.append(idQueue, "Queue", "Default Printer Properties", queueBitmap)
 
 # menu for when you right click on tray icon
 var trayMenu = Menu() 
 trayMenu.appendSeparator()
-trayMenu.appendSubMenu(menuDefaultPrinter, get_default_printer(), "Default Printer")
+trayMenu.appendSubMenu(menuDefaultPrinter, get_default_printer(), "Default Printer", rightBitmap)
 trayMenu.appendSeparator()
 trayMenu.append(idExit, "E&xit", "Exit the program.", exitBitmap)
 
 # iterate over local printers and network printers we've made a connection to
 var menuPrinters = Menu()
 var needed, returned: DWORD
-EnumPrinters(PRINTER_ENUM_CONNECTIONS or PRINTER_ENUM_LOCAL, nil, 1, nil, 0, &needed, &returned)
+EnumPrinters(PRINTER_ENUM_CONNECTIONS or PRINTER_ENUM_LOCAL, nil, 2, nil, 0, &needed, &returned)
 var buffer = newString(needed)
 let pInfo = cast[LPBYTE](&buffer)
-let printers = cast[ptr UncheckedArray[PRINTER_INFO_1]](pInfo)
-EnumPrinters(PRINTER_ENUM_CONNECTIONS or PRINTER_ENUM_LOCAL, nil, 1, pInfo, needed, &needed, &returned)
+let printers = cast[ptr UncheckedArray[PRINTER_INFO_2]](pInfo)
+EnumPrinters(PRINTER_ENUM_CONNECTIONS or PRINTER_ENUM_LOCAL, nil, 2, pInfo, needed, &needed, &returned)
 
-var printer_names: seq[string]
-for i in 0..<returned:
-  printer_names.add($(printers[i].pName))
-printer_names.sort()
+type PrinterEntry = object
+  name: string
+  attributes: DWORD
+  portName: string
 
+var printer_entries: seq[PrinterEntry]
 for i in 0..<returned:
-  menuPrinters.appendRadioItem(wCommandID i, printer_names[i], printer_names[i])  
+  printer_entries.add(PrinterEntry(
+    name: $(printers[i].pPrinterName),
+    attributes: printers[i].Attributes,
+    portName: $(printers[i].pPortName)
+  ))
+
+# sort by name
+printer_entries.sort(proc(a, b: PrinterEntry): int = cmp(a.name, b.name))
+
+for i in 0..<printer_entries.len:
+  menuPrinters.append(wCommandID i, printer_entries[i].name, printer_entries[i].name, printerBitmap)  
   
 # insert sub menu containing printers
 trayMenu.insertSubMenu(0, menuPrinters, "Set Printer", "Change Default Printer.", printerBitmap)
 
 proc update_printer_menu(): void = # check default printer and mark it checked
   var default = get_default_printer()
+  # uncheck all items first
+  for i in trayMenu.getSubMenu(0):
+    i.check(false)
+  # check the default printer
   for i in trayMenu.getSubMenu(0):
     if $(i.text) == default:
-      i.check()
+      i.check(true)
 
 frame.idExit do ():
   frame.delete
